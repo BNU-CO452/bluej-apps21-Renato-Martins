8123470b33e6200ac36db8e406153332ab4139c0
Ren <renato.martins39@gmail.com>
2021-11-17 05:33:05 +0000

    Upload

--- app213-products/StockDemo.java
+++ app213-products/StockDemo.java
@@ -1,4 +1,4 @@
-
+import java.util.Random;
 /**
  * Demonstrate the StockManager and Product classes.
  * The demonstration becomes properly functional as
@@ -12,6 +12,8 @@ public class StockDemo
     // The stock manager.
     private StockList stock;
 
+    private Random random; 
+    
     /**
      * Create a StockManager and populate it with at least
      * 10 sample products.
@@ -19,14 +21,23 @@ public class StockDemo
     public StockDemo(StockList stock)
     {
         this.stock = stock;
-        
+        this.random = new Random();
         // Add at least 10 products, they must be unique to you
         // Make sure the ids are sequential numbers
         
-        stock.add(new Product(101, "Samsung Galaxy S20"));
-        stock.add(new Product(102, "Apple iPhone 12"));
-        stock.add(new Product(103, "Google Pixel 4A"));
+        stock.add(new Product(101, "Assassin's Creed"));
+        stock.add(new Product(102, "Assassin's Creed II"));
+        stock.add(new Product(103, "Battlefield 4"));
+        stock.add(new Product(104, "Fortnite"));
+        stock.add(new Product(105, "Among Us"));
+        stock.add(new Product(106, "Assassin's Creed IV: Black Flag"));
+        stock.add(new Product(107, "Black Desert"));
+        stock.add(new Product(108, "COD Vanguard"));
+        stock.add(new Product(109, "Halo Infinite"));
+        stock.add(new Product(110, "Elden Ring"));
+         
     }
+    
     
     /**
      * Provide a demonstration of how the ProductList meets all
@@ -47,12 +58,32 @@ public class StockDemo
         sellProducts();
         stock.print();        
     }
-    
+    // This method buys random ammounts of each of the products
     private void buyProducts()
     {
+        stock.buyProduct(101, 5);
+        stock.buyProduct(102, 8);
+        stock.buyProduct(103, 8);
+        stock.buyProduct(104, 7);
+        stock.buyProduct(105, 9);
+        stock.buyProduct(106, 3);
+        stock.buyProduct(107, 9);
+        stock.buyProduct(108, 5);
+        stock.buyProduct(109, 6);
+        stock.buyProduct(110, 6);
     }
-
+    // This method sells random ammounts of each of the products
     private void sellProducts()
     {
+        stock.sellProduct(101, 4);
+        stock.sellProduct(102, 6);
+        stock.sellProduct(103, 7);
+        stock.sellProduct(104, 5);
+        stock.sellProduct(105, 7);
+        stock.sellProduct(106, 2);
+        stock.sellProduct(107, 8);
+        stock.sellProduct(108, 4);
+        stock.sellProduct(109, 4);
+        stock.sellProduct(110, 3);
     }    
 }
--- app213-products/StockList.java
+++ app213-products/StockList.java
@@ -11,7 +11,7 @@ public class StockList
 {
     // A list of the products.
     private ArrayList<Product> stock;
-
+        
     /**
      * Initialise the stock manager.
      */
@@ -30,13 +30,29 @@ public class StockList
     }
     
     /**
+     * remove a product from the list.
+     */
+    public void remove(int productID)
+    {
+        Product product = findProduct(productID);
+        if(product !=null)
+            {
+                System.out.println(product+ "has been removed");   
+                stock.remove(product); 
+            }      
+        else
+            {
+                System.out.println("no item with that ID exist");
+            }
+    }
+    
+    /**
      * A method to buy a single quantity of the product
      */
     public void buyProduct(int productID)
     {
         buyProduct(productID, 1);
     }
-    
     
     /**
      * Buy a quantity of a particular product.
@@ -46,6 +62,25 @@ public class StockList
      */
     public void buyProduct(int productID, int amount)
     {
+        Product product = findProduct(productID);
+        if(product != null) 
+        {
+            if(product.getQuantity() < 100)
+            {
+                product.increaseQuantity(amount);
+                System.out.println(" Bought " + amount + " of " + product.getName());
+
+            }
+            else
+            {
+                System.out.println("Not enough space for " + product.getName() + 
+                    " please sell more stock ");
+            }
+        }
+        else
+        {
+            System.out.println("No product found");
+        }
     }
     
     /**
@@ -54,35 +89,54 @@ public class StockList
      */
     public Product findProduct(int productID)
     {
+        for(Product product : stock)
+        
+        {
+            if(product.getID() == productID)
+                return product;
+        }
         return null;
     }
     
+    /**
+     * Sell one of the given product.
+     */
+    public void sellProduct(int productID)
+    {
+        sellProduct(productID, 105);
+    }
     
     /**
      * Sell one of the given product.
      * Show the before and after status of the product.
      * @param id The ID of the product being sold.
      */
-    public void sellProduct(int productID)
+    public void sellProduct(int productID, int amount)
     {
         Product product = findProduct(productID);
-        
+
         if(product != null) 
         {
-            if(product.getQuantity() > 0)
+            if(product.getQuantity() > 0 && product.getQuantity() > amount)
             {
-                product.decreaseQuantity(1);
-                
-                // printout message
+                product.decreaseQuantity(amount);
+                System.out.println(" Sold " + amount + " of " + product.getName());
+
+            }
+            else if (product.getQuantity() == 0)
+            {
+                System.out.println("The product" + product.getName() + 
+                    "is out of stock");
             }
             else
             {
-                // printout message
+                System.out.println("Not able to sell " + amount + " of " + product.getName() + 
+                    "because stock level is " + product.getQuantity()); 
             }
         }
         else
         {
-            // printout message
+            System.out.println("Product not found");
         }
     }    
 
@@ -96,6 +150,9 @@ public class StockList
      */
     public int numberInStock(int productID)
     {
+        Product product = findProduct(productID);
+        if(product !=null)
+             System.out.println(product.toString());
         return 0;
     }
 
@@ -133,8 +190,38 @@ public class StockList
     public void printHeading()
     {
         System.out.println();
-        System.out.println(" Peacock's Stock List");
+        System.out.println(" Ren's Stock List");
         System.out.println(" ====================");
         System.out.println();
+    }
+    
+    /**
+    * Search for a product
+    */
+    public void searchProducts(String phrase)
+    {
+        for(Product product : stock)
+        {
+            if(product.getName().startsWith(phrase) == true)
+            {
+                System.out.println(product);
+            }
+        }
+    }
+    
+    /**
+    * Show Stock under 5
+    */
+    public void stockLower5()
+    {
+        for(Product product : stock)
+        {
+            if(product.getQuantity() < 5 )
+            {
+                System.out.println(" Stock Below 5 ");
+                System.out.println(" ------------- ");
+                System.out.println(product);
+            }
+        }
     }
 }
